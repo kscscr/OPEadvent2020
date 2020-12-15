aed5c7d350d1987331be291019163aac86c325d3
kscscr <63399643+kscscr@users.noreply.github.com>
2020-12-16 02:04:30 +0900

    Add files via upload

--- jspsych-6.1.0/plugins/jspsych-fullscreen.js
+++ jspsych-6.1.0/plugins/jspsych-fullscreen.js
@@ -23,7 +23,7 @@ jsPsych.plugins.fullscreen = (function() {
       message: {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Message',
-        default: '<p>次へをクリックするとと、画面がフルスクリーンモードへと切り替わります。</p>',
+        default: '<p>次へをクリックすると、画面がフルスクリーンモードへと切り替わります。</p>',
         array: false,
         description: 'HTML content to display above the button to enter fullscreen mode.'
       },
